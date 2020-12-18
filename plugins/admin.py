6e8ac89de8d8bd7d3e9f5d08bc0f09605046ea9d
Plato <platoo@outlook.it>
2020-12-18 04:36:01 +0100

    Add commands to join and leave rooms

--- plugins/admin.py
+++ plugins/admin.py
@@ -3,6 +3,8 @@ from __future__ import annotations
 import asyncio
 from typing import TYPE_CHECKING
 
+import utils
+from models.room import Room
 from plugins import command_wrapper
 
 if TYPE_CHECKING:
@@ -15,3 +17,34 @@ async def kill(msg: Message) -> None:
         for task in asyncio.all_tasks(loop=msg.conn.loop):
             task.cancel()
         await msg.conn.websocket.close()
+
+
+@command_wrapper()
+async def join(msg: Message) -> None:
+    roomid = utils.to_room_id(msg.arg)
+    if not roomid:
+        await msg.reply("Inserire un nome valido per la room")
+        return
+
+    room = Room.get(msg.conn, roomid)
+    if not (msg.user.is_administrator or msg.user.has_role("owner", room)):
+        return
+
+    if not await room.join():
+        await msg.reply("Impossibile joinare la room")
+
+
+@command_wrapper()
+async def leave(msg: Message) -> None:
+    if msg.room is not None and not msg.arg:
+        room = msg.room
+    else:
+        roomid = utils.to_room_id(msg.arg)
+        if not roomid:
+            await msg.reply("Inserire un nome valido per la room")
+            return
+
+        room = Room.get(msg.conn, roomid)
+
+    if not await room.leave():
+        await msg.reply("Impossibile leftare la room")
